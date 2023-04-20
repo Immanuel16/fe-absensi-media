20771e0728c416475a563b0ff86ee65d6b6820fe
jiraiya <nuel@gbirayon18.org>
2023-04-20 14:37:16 +0700

    update bank account feature

--- src/pages/Account/BankAccount/BankAccount.js
+++ src/pages/Account/BankAccount/BankAccount.js
@@ -1,13 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BackIcon } from "../../../assets/icons";
 import { useNavigate } from "react-router";
 import { useUser } from "../../../context/Auth";
 import { DefaultAva } from "../../../assets";
-import { Input } from "antd";
+import { Input, Select } from "antd";
+import { useFormik } from "formik";
+import {
+  BankCrewSchemas,
+  InitialValuesBankCrew,
+} from "../../../schemas/UserSchemas";
+import apiHelper from "../../../helper/api";
+import { ErrorMessage } from "../../../components/ErrorMessage/ErrorMessage";
+import { useSpinner } from "../../../context/Spinner";
+import { showAlertSuccess } from "../../../util/util";
 
 const BankAccount = () => {
   const navigate = useNavigate();
-  const { user } = useUser();
+  const { setShowSpinner } = useSpinner();
+  const { user, setUser } = useUser();
+  const [isEdit, setIsEdit] = useState(false);
+  const [listBank, setListBank] = useState([]);
+
+  const formik = useFormik({
+    initialValues: InitialValuesBankCrew,
+    validationSchema: BankCrewSchemas,
+
+    onSubmit: (values) => {
+      saveData(values);
+    },
+  });
+
+  const getListBank = async () => {
+    try {
+      const { data } = await apiHelper.get("/public/bank-account");
+      const list = data.map((d) => ({
+        value: d.bank_code,
+        label: d.bank_name,
+      }));
+      setListBank(list);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getListBank();
+  }, []);
+
+  const getDataUser = async () => {
+    try {
+      const { data } = await apiHelper.get("/apps/accounts");
+      setUser(data);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    onChangeField("bank_name", user.bank_id);
+    onChangeField("bank_acc_num", user.bank_acc_num);
+    onChangeField("bank_acc_name", user.bank_acc_name);
+  }, [user]);
+
+  const onChangeField = (name, e) => {
+    formik.setFieldValue(name, e);
+  };
+
+  const onBlurField = (name) => {
+    formik.setFieldTouched(name, true, true);
+  };
+
+  const saveData = async (data) => {
+    setShowSpinner(true);
+    apiHelper.put(`/apps/accounts/edit/bank-account`, data).then(() => {
+      setShowSpinner(false);
+      showAlertSuccess("Akun bank berhasil diubah");
+      setIsEdit((prev) => !prev);
+      getDataUser();
+    });
+  };
+
+  const editData = () => {
+    setShowSpinner(true);
+    setIsEdit((prev) => !prev);
+    setTimeout(() => {
+      setShowSpinner(false);
+    }, 1250);
+  };
+
+  const onClickButton = () => {
+    isEdit ? saveData(formik.values) : editData();
+  };
+
+  const onClickBtnHeader = () => {
+    !isEdit ? navigate(-1) : setIsEdit((prev) => !prev);
+  };
 
   return (
     <>
@@ -20,11 +107,25 @@ const BankAccount = () => {
             borderBottomRightRadius: "20px",
           }}
         >
-          <button onClick={() => navigate(-1)}>
-            <BackIcon />
+          <button
+            onClick={onClickBtnHeader}
+            className="text-white text-base font-medium"
+            type="button"
+          >
+            {!isEdit ? <BackIcon /> : "Close"}
           </button>
           <p>Akun Bank</p>
-          <p></p>
+          {!isEdit ? (
+            <p></p>
+          ) : (
+            <button
+              type="submit"
+              className="text-white text-base font-medium"
+              onClick={() => saveData(formik.values)}
+            >
+              Save
+            </button>
+          )}
         </div>
 
         {/* main */}
@@ -58,25 +159,66 @@ const BankAccount = () => {
               </label>
               <Input
                 className="text-media-black-4 border-media-secondary-gray"
-                value={user.bank_acc_num}
-                readOnly
+                value={formik.values.bank_acc_num}
+                readOnly={!isEdit}
+                onChange={formik.handleChange}
+                name="bank_acc_num"
               />
             </div>
 
             {/* Nama Bank */}
-            <div className="flex flex-col space-y-1">
-              <label
-                htmlFor="norek"
-                className="font-semibold text-media-black-3"
-              >
-                Nama Bank
-              </label>
-              <Input
-                className="text-media-black-4 border-media-secondary-gray"
-                value={user.bank_name}
-                readOnly
-              />
-            </div>
+            {!isEdit ? (
+              <div className="flex flex-col space-y-1">
+                <label
+                  htmlFor="norek"
+                  className="font-semibold text-media-black-3"
+                >
+                  Nama Bank
+                </label>
+                <Input
+                  className="text-media-black-4 border-media-secondary-gray"
+                  value={user.bank_name}
+                  readOnly
+                />
+              </div>
+            ) : (
+              <div className="flex flex-col space-y-1">
+                <label
+                  htmlFor="bank_id"
+                  // className={`${formik.touched.bank_id && formik.errors.bank_id ? 'text-media-danger-3': 'text-media-primary-gray'}`}
+                >
+                  Nama Bank
+                </label>
+                <div
+                  className={`border rounded-lg ${
+                    formik.touched.bank_id && formik.errors.bank_id
+                      ? "border-media-danger-3"
+                      : "border-media-primary-gray"
+                  } w-full`}
+                >
+                  <Select
+                    bordered={false}
+                    showSearch
+                    onBlur={() => onBlurField("bank_id")}
+                    placeholder="Pilih nama bank"
+                    optionFilterProp="children"
+                    onChange={(value) => onChangeField("bank_name", value)}
+                    filterOption={(input, option) =>
+                      (option?.label ?? "")
+                        .toLowerCase()
+                        .includes(input.toLowerCase())
+                    }
+                    options={listBank}
+                    className="w-full"
+                    value={formik.values.bank_name}
+                  />
+                </div>
+                <ErrorMessage
+                  show={formik.touched.bank_id && formik.errors.bank_id}
+                  message={formik.errors.bank_id}
+                />
+              </div>
+            )}
 
             {/* Atas Nama */}
             <div className="flex flex-col space-y-1">
@@ -88,10 +230,32 @@ const BankAccount = () => {
               </label>
               <Input
                 className="text-media-black-4 border-media-secondary-gray"
-                value={user.bank_acc_name}
-                readOnly
+                value={formik.values.bank_acc_name}
+                readOnly={!isEdit}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                name="bank_acc_name"
               />
             </div>
+          </div>
+
+          {/* Edit Data */}
+          <div className="flex flex-col space-y-4 w-full items-center mt-6">
+            {/* button edit data */}
+            {!isEdit && (
+              <button
+                className="border border-salestrack-primary-blue rounded-xl text-media-primary-blue font-semibold text-sm w-4/5 py-1.5"
+                onClick={onClickButton}
+              >
+                {isEdit ? "Simpan" : "Edit Data"}
+              </button>
+            )}
+
+            {/* divider */}
+            <div
+              className="border-2 border-media-secondary-gray opacity-40"
+              style={{ width: "20%" }}
+            ></div>
           </div>
         </div>
       </div>
--- src/schemas/UserSchemas.js
+++ src/schemas/UserSchemas.js
@@ -1,4 +1,5 @@
-export const InitialValuesCrew = {
+import * as Yup from "yup";
+const InitialValuesCrew = {
   id: "",
   full_name: "",
   birth_date: "",
@@ -21,3 +22,21 @@ export const InitialValuesCrew = {
   role: "",
   email: "",
 };
+
+const InitialValuesBankCrew = {
+  bank_name: "",
+  bank_acc_num: "",
+  bank_acc_name: "",
+};
+
+const BankCrewSchemas = Yup.object({
+  bank_name: Yup.string().required("Nama bank wajib diisi"),
+  bank_acc_num: Yup.string()
+    .required("Nomor rekening wajib diisi")
+    .min(10, "Nomor rekening minimal 10 angka"),
+  bank_acc_name: Yup.string()
+    .required("Nama pemilik rekening wajib diisi")
+    .matches(/^[a-z A-Z]+$/, "Nama pemilik rekening wajib huruf"),
+});
+
+export { InitialValuesCrew, InitialValuesBankCrew, BankCrewSchemas };
